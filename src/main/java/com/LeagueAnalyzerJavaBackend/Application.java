f0e2c1637f77b3bec8597bae0a6246669efdee09
Hsieh11 <Chrishsieh@live.com>
2017-04-24 22:23:42 -0400

    summonerSimple controller returns firebun

--- src/main/java/com/LeagueAnalyzerJavaBackend/Application.java
+++ src/main/java/com/LeagueAnalyzerJavaBackend/Application.java
@@ -7,27 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 
-import net.rithms.riot.api.RiotApi;
-import net.rithms.riot.api.RiotApiException;
-import net.rithms.riot.constant.Region;
-import net.rithms.riot.constant.Season;
-import net.rithms.riot.dto.Stats.RankedStats;
-import net.rithms.riot.dto.Summoner.Summoner;
-
-import com.google.gson.*;
-
 @SpringBootApplication
 @ComponentScan("com.SummonerController")
 public class Application {
 
-	public static void main(String[] args) throws RiotApiException {
+	public static void main(String[] args)  {
 		SpringApplication.run(Application.class, args);
 		
-		RiotApi api = new RiotApi("RGAPI-6ec47d55-1394-4e4c-b524-738fe84e6da1", Region.NA);
-		api.setRegion(Region.NA);
-		Map<String, Summoner> summoners = api.getSummonersByName("rithms, tryndamere");
-		Summoner summoner = summoners.get("rithms");
-		long id = summoner.getId();
-		System.out.println(id);
+
 	}
 }
--- src/main/java/com/SummonerController/summonerController.java
+++ src/main/java/com/SummonerController/summonerController.java
@@ -1,12 +1,24 @@
 package com.SummonerController;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.DataRepresentations.Summoner;
+
+
+
+import net.rithms.riot.api.RiotApi;
+import net.rithms.riot.api.RiotApiException;
+import net.rithms.riot.constant.Region;
+import net.rithms.riot.constant.Season;
+import net.rithms.riot.dto.Stats.RankedStats;
+import net.rithms.riot.dto.Summoner.Summoner;
+import com.google.gson.*;
+
 
 @Controller
 @RequestMapping("/summonerSimple")
@@ -15,7 +27,17 @@ public class summonerController {
     private static final String template = "Hello, %s!";
 
     @RequestMapping(method=RequestMethod.GET)
-    public @ResponseBody Summoner sayHello(@RequestParam(value="name", required=false, defaultValue="Stranger") String name) {
-        return new Summoner(1, String.format(template, name));
+    public @ResponseBody Summoner sayHello(@RequestParam(value="name", required=false, defaultValue="Stranger") String name) throws RiotApiException {
+    	
+		RiotApi api = new RiotApi("RGAPI-a22eb6a6-8081-451a-941a-0b7cda5374a3", Region.NA);
+		api.setRegion(Region.NA);
+		Map<String, Summoner> summoners = api.getSummonersByName("firebun, TF Support bby");
+		Summoner summoner = summoners.get("firebun");
+		long id = summoner.getId();
+		System.out.println(id);
+
+    	
+    	
+        return summoner;
     }
 }
